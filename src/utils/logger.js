1c29e2a8adcff7c91848d143f2a606ea6834a0bd
VSAlmeida <vsalmeida@ibm.com>
2024-08-05 10:01:22 -0300

    feat: Add logger utility
    Adds a logger utility to the project, providing functions for logging information, success, warnings, errors, and debug messages. The logger utility uses chalk and ora packages to provide colorful and interactive logging experience.

---
+++ src/utils/logger.js
@@ -0,0 +1,43 @@
+const chalk = require('chalk');
+const ora = require('ora');
+
+const { program } = require('../config');
+
+// eslint-disable-next-line
+const log = console.log;
+
+const spinner = (msg) => {
+  return ora({
+    text: chalk.whiteBright(msg),
+    color: 'white',
+    spinner: 'dots',
+  }).start();
+};
+
+const info = (msg) => {
+  log(chalk.whiteBright(msg));
+};
+
+const success = (msg) => {
+  log(chalk.green(msg));
+};
+
+const warning = (msg) => {
+  log(chalk.yellow(msg));
+};
+
+const error = (msg) => {
+  log(chalk.red(msg));
+};
+
+const debug = (msg) => {
+  const option = program.opts();
+
+  if (!option.debug) {
+    return;
+  }
+
+  log(chalk.blue(msg));
+};
+
+module.exports = { info, success, warning, error, debug, spinner };
