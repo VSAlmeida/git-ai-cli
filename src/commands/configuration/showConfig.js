a5a46f1cd5fccff54b9751dd8f8a7fcd4e85b776
VSAlmeida <vsalmeida@ibm.com>
2024-08-09 10:21:19 -0300

    feat: Add error handling for missing LLM provider configuration
    Added error handling to showConfig command to handle cases where no LLM provider is configured.
    This change improves the user experience by providing clear instructions on how to configure an LLM provider when none is present.

--- src/commands/configuration/showConfig.js
+++ src/commands/configuration/showConfig.js
@@ -3,9 +3,18 @@ const { logger, getConfig } = require('../../utils');
 const showConfig = () => {
   const config = getConfig('current');
 
+  if (!config.current) {
+    logger.error('You dont have any LLM Provider configured');
+    logger.warning('Please run "git ai --config" to configure');
+
+    return;
+  }
+
+  const llmConfig = config.options.find((opt) => opt.name === config.current);
+
   logger.success('Your current configurations:');
 
-  logger.info(JSON.stringify(config, null, 2));
+  logger.info(JSON.stringify(llmConfig, null, 2));
 };
 
 module.exports = showConfig;
