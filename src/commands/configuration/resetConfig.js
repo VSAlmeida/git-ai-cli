6b2067341464c26c8bf337134faaec2d5a6e447f
VSAlmeida <vsalmeida@ibm.com>
2024-08-12 01:32:00 -0300

    feat: Add confirmation prompt to resetConfig command
    Adds a confirmation prompt to the resetConfig command to prevent accidental deletion of credentials.

--- src/commands/configuration/resetConfig.js
+++ src/commands/configuration/resetConfig.js
@@ -1,13 +1,33 @@
+const { confirm } = require('@inquirer/prompts');
+
 const { logger, getConfig, saveConfig } = require('../../utils');
 
-const resetConfig = () => {
+const resetConfig = async () => {
   logger.debug('Resetting configuration file...');
+
+  logger.warning(
+    'This action will permanently delete all your credentials and cannot be undone.'
+  );
+
+  const answer = await confirm({
+    message:
+      'Are you sure you want to proceed with resetting the configuration?',
+    default: false,
+  });
+
+  if (!answer) {
+    logger.success('\nReset operation canceled');
+
+    return;
+  }
 
   const defaultConfig = getConfig('default');
 
   saveConfig(defaultConfig);
 
-  logger.success('Configuration has been reset to default values');
+  logger.success(
+    '\nConfiguration has been successfully reset to default values'
+  );
 };
 
 module.exports = resetConfig;
